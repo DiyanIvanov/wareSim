6616b27af36cb6c0e643825fa38613efc9a9c34a
Diyan Ivanov <coldys_@hotmail.com>
2026-05-20 08:41:50 +0000

    feat: add Picker worker skeleton

--- core/workers.py
+++ core/workers.py
@@ -4,11 +4,12 @@ import simpy
 
 class Worker(ABC):
 
-    def __init__(self, env, short_brake = 15, lunch_break = 30):
+    def __init__(self, env, warehouse_lauout, short_brake = 15, lunch_break = 30):
         self.env = env
         self.action = env.process(self.run())
         self.short_break = short_brake
         self.lunch_break = lunch_break
+        self.warehouse_lauout = warehouse_lauout
 
     @abstractmethod
     def run(self):
@@ -20,4 +21,28 @@ class Worker(ABC):
 
     def get_lunch_break(self):
         yield self.env.timeout(self.lunch_break)
+
+
+class Picker(Worker):
+
+    TAKE_EQUPMENT_RATE = 5
+    PICK_RATE = 1   
+
+    def __init__(self, env, warehouse_lauout, short_brake = 15, lunch_break = 30):
+        super().__init__(env, warehouse_lauout, short_brake, lunch_break)
+        self.order = None
+
+    def run(self):
+        while True:
+            print(f"Start at {self.env.now}")
+            yield self.env.timeout(5)
+
+    def pick_item(self):
+        pass
+
+    def travel_to(self, location):
+        pass
+
+    def add_empty_container(self):
+        pass    
 
